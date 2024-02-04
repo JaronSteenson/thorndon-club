39ad993ae0fe095c64657a95846d2fb6831e16bd
Jaron Steenson <jaron.steenson@kitomba.com>
2024-02-05 00:14:56 +1300

    Add mailchimp subscribe form

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import MobileLaMailchimpSubscribeForm from '@/app/_components/MailchimpSubscribeForm';
+
 export default function Home() {
-    return 'Stuff';
+    return (
+        <>
+            <section className="w-96">
+                <h2>Subscribe to our mailing list</h2>
+                <MobileLaMailchimpSubscribeForm />
+            </section>
+        </>
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import colors from 'tailwindcss/colors';
 
 const config: Config = {
     content: [
@@ -11,6 +12,11 @@ const config: Config = {
             backgroundImage: {
                 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
                 'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            colors: {
+                primary: colors.blue,
+                secondary: colors.yellow,
+                neutral: colors.gray,
             },
         },
     },
